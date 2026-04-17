634fdc1d206926e6788f9bb10b0adb548d10e8f0
Arashxol <joelcitochoquecito76@gmail.com>
2026-04-16 23:56:57 -0400

    ahora_si

--- Assets/Scripts/OpcionesMenu.cs
+++ Assets/Scripts/OpcionesMenu.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class OpcionesMenu : MonoBehaviour
@@ -5,5 +6,11 @@ public class OpcionesMenu : MonoBehaviour
 public void vamosAjugar()
     {
         SceneManager.LoadScene(1);
+    }
+public void Salir()
+    {
+        //para la consola en Unity
+        
+        Application.Quit();
     }
 }
